0f4276e22a5b39b92e14ece5b24c6209556c0dab
antirez <antirez@gmail.com>
2017-07-08 10:59:59 +0200

    Can play a meaningful game.

--- c4.c
+++ c4.c
@@ -1,41 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define INVALID_CIRCLE -1
-#define EMPTY_CIRCLE 0
-#define YELLOW_CIRCLE 1
-#define RED_CIRCLE 2
+/* Return values: ivalid, empty circle, red or yellow player, or game draw. */
+#define CIRCLE_INVALID -1
+#define CIRCLE_EMPTY 0
+#define CIRCLE_YELLOW 1
+#define CIRCLE_RED 2
+#define CIRCLE_DRAW 3
 
+/* Our 6x7 board, it's composed of 7 columns (0 to 6 from left to right) and
+ * 6 levels (0 to 5 from bottom to top)
+
+ * [.][.][.][.][.][.][.] level 5
+ * [.][.][.][.][.][.][.] level 4
+ * [.][.][.][R][.][.][.] level 3
+ * [.][.][.][R][.][.][.] level 2
+ * [.][.][.][R][Y][.][.] level 1
+ * [.][.][.][R][Y][.][.] level 0
+ *  0  1  2  3  4  5  6
+ */
 typedef struct board {
     char holes[6*7];
 } board;
 
+/* Return what the specified col/level hole contains. */
 int c4Get(board *b, int col, int level) {
-    if (col < 0 || col > 6 || level < 0 || level > 5) return INVALID_CIRCLE;
+    if (col < 0 || col > 6 || level < 0 || level > 5) return CIRCLE_INVALID;
     return b->holes[(5*7)+col-(level*7)];
 }
 
+/* Set the specified col/level hole to the specified circle value, see
+*  the CIRCLE_* macros. */
 void c4Set(board *b, int col, int level, int value) {
     if (col < 0 || col > 6 || level < 0 || level > 5) return;
     b->holes[(5*7)+col-(level*7)] = value;
 }
 
+/* Clean the board filling it with empty values. */
 void c4Clean(board *b) {
-    memset(b->holes,EMPTY_CIRCLE,sizeof(b->holes));
+    memset(b->holes,CIRCLE_EMPTY,sizeof(b->holes));
 }
 
+/* Copy the source board to the destination board. */
 void c4Copy(board *dst, board *src) {
     memcpy(dst->holes,src->holes,sizeof(dst->holes));
 }
 
+/* Return true if the given colum is all full with circles. */
 int c4ColIsFull(board *b, int col) {
-    return c4Get(b,col,5) != EMPTY_CIRCLE;
+    return c4Get(b,col,5) != CIRCLE_EMPTY;
 }
 
+/* Drop a circle in the give column. If it's full 0 is returned, otherwise
+ * the first empty level is set to the specified value. */
 int c4Drop(board *b, int col, int value) {
     if (c4ColIsFull(b,col)) return 0;
     for (int level = 0; level < 6; level++) {
-        if (c4Get(b,col,level) == EMPTY_CIRCLE) {
+        if (c4Get(b,col,level) == CIRCLE_EMPTY) {
             c4Set(b,col,level,value);
             break;
         }
@@ -43,22 +66,148 @@ int c4Drop(board *b, int col, int value) {
     return 1;
 }
 
+/* Print an ASCII art representation of the board. */
 void c4Print(board *b) {
     for (int level = 5; level >= 0; level--) {
         for (int col = 0; col < 7; col++) {
-            int circle = c4Get(b,col,level);
+            int color = c4Get(b,col,level);
             char *set = ".YR";
-            printf("[%c]",set[circle]);
+            printf("[%c]",set[color]);
         }
         printf("\n");
+    }
+    for (int col = 0; col < 7; col++)
+        printf(" %d ",col);
+    printf("\n");
+}
+
+/* Return the winner color, or CIRCLE_EMPTY if there is no winner.
+ * If the board is full but there is no winner, CIRCLE_DRAW is returned. */
+int c4GetWinner(board *b) {
+    int empty = 0;
+    for (int level = 5; level >= 0; level--) {
+        for (int col = 0; col < 7; col++) {
+            int color = c4Get(b,col,level);
+            if (color == CIRCLE_EMPTY) {
+                empty++;
+                continue;
+            }
+
+            /* There are four possible valid directions to make
+             * circles in a row: */
+            struct {
+                int col_incr;
+                int level_incr;
+            } dir[4] = {
+                {1,0},
+                {0,1},
+                {1,1},
+                {-1,1}
+            };
+
+            /* Check if from this position there is a winning sequence. */
+            for (int d = 0; d < 4; d++) {
+                /* Seek the initial circle still having this color. */
+                int start_col = col;
+                int start_level = level;
+                while(c4Get(b,start_col-dir[d].col_incr,
+                              start_level-dir[d].level_incr) == color)
+                {
+                    start_col -= dir[d].col_incr;
+                    start_level -= dir[d].level_incr;
+                }
+
+                int count = 0; /* Count consecutive circles. */
+                while(c4Get(b,start_col,start_level) == color) {
+                    count++;
+                    start_col += dir[d].col_incr;
+                    start_level += dir[d].level_incr;
+                }
+                if (count >= 4) return color;
+            }
+        }
+    }
+    return empty ? CIRCLE_EMPTY : CIRCLE_DRAW;
+}
+
+/* Simulate a random game. The 'tomove' argument tells the function who
+ * is the player to move first. The board does not need to be empty, the random
+ * game is continued from the current state. The function returns the winner
+ * color, or CIRCLE_DRAW if there is no winner. */
+int c4RandomGame(board *b, int tomove) {
+    while(1) {
+        if (c4Drop(b,rand()%7,tomove)) {
+            tomove = (tomove == CIRCLE_YELLOW) ? CIRCLE_RED : CIRCLE_YELLOW;
+        }
+        int winner = c4GetWinner(b);
+        if (winner != CIRCLE_EMPTY) return winner;
+    }
+}
+
+/* Suggest a move among the (at max) 7 possible. For each move we play random games
+ * and store the ratio between won and lost games. Finally the move with the best
+ * ratio is returned. If there is no valid moves, -1 is returned. */
+int c4SuggestMove(board *b, int tomove) {
+    int best = -1;
+    double best_ratio = 0;
+    int games_per_move = 10000;
+    for (int move = 0; move < 7; move++) {
+        if (c4ColIsFull(b,move)) continue; /* No valid move. */
+        int won = 0, lost = 0;
+        for (int j = 0; j < games_per_move; j++) {
+            board copy;
+            c4Copy(&copy,b);
+            c4Drop(&copy,move,tomove);
+            tomove = (tomove == CIRCLE_YELLOW) ? CIRCLE_RED : CIRCLE_YELLOW;
+            int winner = c4RandomGame(&copy,tomove);
+            if (winner == CIRCLE_YELLOW || winner == CIRCLE_RED) {
+                if (winner != tomove) {
+                    won++;
+                } else {
+                    lost++;
+                }
+            }
+        }
+        double ratio = (double)won/(lost+1);
+        printf("Move %d ratio: %f\n", move, ratio);
+        if (ratio > best_ratio || best == -1) {
+            best = move;
+            best_ratio = ratio;
+        }
+    }
+    return best;
+}
+
+/* Play interactively with the user. */
+void c4play(void) {
+    board b;
+    c4Clean(&b);
+
+    while(1) {
+        c4Print(&b);
+        if (c4GetWinner(&b) != CIRCLE_EMPTY) break;
+
+        int human_move = -1;
+        while(human_move == -1) {
+            printf("Red, state your move: ");
+            fflush(stdout);
+            char buf[32];
+            fgets(buf,sizeof(buf),stdin);
+            human_move = atoi(buf);
+
+            /* Check the validity. */
+            if (human_move > 6) human_move = -1;
+            if (human_move < 0) human_move = -1;
+            if (human_move != -1 && c4ColIsFull(&b,human_move)) human_move = -1;
+        }
+        c4Drop(&b,human_move,CIRCLE_RED);
+
+        int computer_move = c4SuggestMove(&b,CIRCLE_YELLOW);
+        c4Drop(&b,computer_move,CIRCLE_YELLOW);
     }
 }
 
 int main(void) {
-    board b;
-    c4Clean(&b);
-    c4Drop(&b,3,RED_CIRCLE);
-    c4Drop(&b,3,RED_CIRCLE);
-    c4Drop(&b,4,YELLOW_CIRCLE);
-    c4Print(&b);
+    srand(time(NULL));
+    c4play();
 }
